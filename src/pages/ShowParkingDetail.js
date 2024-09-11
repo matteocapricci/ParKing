62c43d4e043ffcbed53f216b86b991f7e070a673
MatteoDRC00 <matteo00drc@gmail.com>
2024-09-11 09:00:56 +0200

    added comments in ShowParkingDetail

--- src/pages/ShowParkingDetail.js
+++ src/pages/ShowParkingDetail.js
@@ -7,11 +7,41 @@ import ParkingCard from "../components/ParkingCard.js";
 import { useSelector } from "react-redux";
 import BackButton from "../components/BackButton.js";
 import ReservationReview from "../components/ReservationReview.js";
+import { get_docs_by_attribute, load_docs_by_attributes, load_docs } from '../services/firebase/crudOp.js';
+import theme from '../style/palette.js';
+import CommentCard from '../components/CommentCard.js';
 
 function ShowParkingDetail(){
 
     const parking = useSelector(state => state.selectedParking.selectedParking);
-    console.log(parking);
+    const [parkComments, setParkComment] = useState([]);
+    
+    const retriveCommentsByParking = async () => {
+
+        try {
+            const retrievedComments = await get_docs_by_attribute(parking.doc_id, "Comment", "parkingId");
+            let newComments = [];
+            
+            if (retrievedComments.length > 0 ){
+                await Promise.all(retrievedComments.map(async (element) => {
+                    const user = await load_docs_by_attributes("UserImage", { "uid": element.uid });
+                    newComments.push({
+                        ...element,
+                        photoURL: user[0]?.photoURL || '', 
+                        displayName: user[0]?.displayName || 'Unknown' 
+                    });
+                }));
+
+                setParkComment(newComments);
+                return true;
+            }
+        } catch (error) {
+            console.error("Failed to retrieve comments:", error);
+            return false;
+        }
+    }
+
+    retriveCommentsByParking();
 
     return (
         <div>
@@ -37,6 +67,22 @@ function ShowParkingDetail(){
                     <ReservationReview />
                 </Grid>
             </Grid>
+            <div>
+            <h1 style={{fontSize: '25px', color: theme.palette.secondary.main, marginLeft: '50px', marginTop: '30px'}}>Comments: </h1>
+            {parkComments.length > 0 ? (
+                parkComments.map((comment, index) => (
+                    <CommentCard 
+                        comment={comment} 
+                        username={comment.displayName}
+                        photoUrl={comment.photoURL} 
+                    />
+                ))
+            ) : (
+                <h1 style={{fontSize: '20px', color: theme.palette.primary.main, marginLeft: '50px', marginTop: '20px'}}>
+                    There are no comments for this parking
+                </h1>
+            )}
+            </div>
             <Footer />
         </div>
         
